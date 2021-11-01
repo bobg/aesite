3221bcc9838a153f53e2a645e430d84596105c08
Bob Glickstein <bobg@emphatic.com>
2021-11-01 07:20:32 -0700

    Update DSTest again to add a time.Sleep in the kill function, and to return some ClientOptions for use in creating the DS connection.

--- dstest.go
+++ dstest.go
@@ -8,10 +8,14 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"sync"
 	"syscall"
 	"time"
 
 	"github.com/pkg/errors"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 var envInitRegex = regexp.MustCompile(`export\s+([^=]+)=(.*)`)
@@ -25,8 +29,9 @@ var envInitRegex = regexp.MustCompile(`export\s+([^=]+)=(.*)`)
 // (again with gcloud)
 // as needed for the datastore client library to use the emulator.
 //
-// Return value is a function that can be used to kill the emulator subprocess and return its Wait result.
-func DSTest(ctx context.Context, projectID string) (func() error, error) {
+// Returns a slice of option.ClientOptions that the caller should use in a call to datastore.NewClient,
+// and a function that can be used to kill the emulator subprocess and return its Wait result.
+func DSTest(ctx context.Context, projectID string) ([]option.ClientOption, func() error, error) {
 	log.Print("starting datastore emulator")
 
 	cmd := exec.CommandContext(ctx, "gcloud", "--project", projectID, "beta", "emulators", "datastore", "start")
@@ -34,14 +39,14 @@ func DSTest(ctx context.Context, projectID string) (func() error, error) {
 
 	err := cmd.Start()
 	if err != nil {
-		return nil, errors.Wrap(err, "starting datastore emulator")
+		return nil, nil, errors.Wrap(err, "starting datastore emulator")
 	}
 
 	time.Sleep(2 * time.Second)
 
 	envLines, err := exec.Command("gcloud", "--project", projectID, "beta", "emulators", "datastore", "env-init").Output()
 	if err != nil {
-		return nil, errors.Wrap(err, "running env-init command")
+		return nil, nil, errors.Wrap(err, "running env-init command")
 	}
 
 	s := bufio.NewScanner(bytes.NewReader(envLines))
@@ -51,20 +56,80 @@ func DSTest(ctx context.Context, projectID string) (func() error, error) {
 		if len(m) >= 3 {
 			err = os.Setenv(m[1], m[2])
 			if err != nil {
-				return nil, errors.Wrapf(err, "setting env var %s to %s", m[1], m[2])
+				return nil, nil, errors.Wrapf(err, "setting env var %s to %s", m[1], m[2])
 			}
 		}
 	}
 
-	return func() error {
-		log.Print("waiting 30 seconds to terminate the datastore emulator")
+	var (
+		mu sync.Mutex
+		t  = time.Now()
+	)
 
-		// TODO: create GRPC interceptors,
-		// and return them from this function for the caller to use when calling datastore.NewClient.
-		// The interceptors will simply note the time of any call to the datastore emulator,
-		// so that the duration of this sleep can be reduced to _at most_ 30 seconds.
-		time.Sleep(30 * time.Second)
+	u := func() {
+		mu.Lock()
+		t = time.Now()
+		mu.Unlock()
+	}
+
+	unaryInterceptor := func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		u()
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+	streamInterceptor := func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+		u()
+		s, err := streamer(ctx, desc, cc, method, opts...)
+		return &dstestClientStream{update: u, s: s}, err
+	}
+	opts := []option.ClientOption{
+		option.WithGRPCDialOption(grpc.WithUnaryInterceptor(unaryInterceptor)),
+		option.WithGRPCDialOption(grpc.WithStreamInterceptor(streamInterceptor)),
+	}
+
+	k := func() error {
+		d := time.Until(t.Add(30 * time.Second))
+		if d > 0 {
+			log.Printf("waiting %s to terminate the datastore emulator", d)
+			time.Sleep(d)
+		}
 		syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL) // kill the whole process group
 		return cmd.Wait()
-	}, nil
+	}
+
+	return opts, k, nil
+}
+
+type dstestClientStream struct {
+	update func()
+	s      grpc.ClientStream
+}
+
+func (s *dstestClientStream) Header() (metadata.MD, error) {
+	s.update()
+	return s.s.Header()
+}
+
+func (s *dstestClientStream) Trailer() metadata.MD {
+	s.update()
+	return s.s.Trailer()
+}
+
+func (s *dstestClientStream) CloseSend() error {
+	s.update()
+	return s.s.CloseSend()
+}
+
+func (s *dstestClientStream) Context() context.Context {
+	s.update()
+	return s.s.Context()
+}
+
+func (s *dstestClientStream) SendMsg(m interface{}) error {
+	s.update()
+	return s.s.SendMsg(m)
+}
+
+func (s *dstestClientStream) RecvMsg(m interface{}) error {
+	s.update()
+	return s.s.RecvMsg(m)
 }
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/bobg/aesite/v3
+module github.com/bobg/aesite/v4
 
 go 1.13
 
